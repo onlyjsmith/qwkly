2bd34ff0ae159903ffbc420d7f9ed90b3b68d848
Jonathan Smith <jonathan@Jonathan-Smiths-Mac-mini.local>
2009-11-23 21:52:58 +0000

    Have got some link between keyword and site going. Need a "through" table though.

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -14,7 +14,6 @@ class KeywordsController < ApplicationController
   # GET /keywords/1.xml
   def show
     @keyword = Keyword.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @keyword }
